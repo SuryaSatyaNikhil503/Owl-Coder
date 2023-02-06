b317056c24b699af7ebe51e33cf94d2f4509d032
SuryaSatyaNikhil503 <102517041+SuryaSatyaNikhil503@users.noreply.github.com>
2023-02-06 21:04:36 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,9 +1,18 @@
 import java.util.*;
 public class Fibonacci
 {
+    static int fibo(int n)
+    {
+        if(n < 2)
+        {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
     }
 }
